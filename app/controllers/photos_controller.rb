6b27f697c0900dfde091a8f5f900b6729b61bbdc
ameliaknipp <ameliaknipp@aol.com>
2019-06-22 19:17:20 +0000

    Debugging migration

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,17 +1,5 @@
 class PhotosController < ApplicationController
 
-  #  def index
-  #  @photos = Photo.all
-  #  @photos = Photo.find(params[:id])
- # end
-
-  #def create
- #   @photos = Photo.new
-  #end
-
-  #def new
-  #  @photo = Photo.new
-  #end
 
   def create
     @place = Place.find(params[:place_id])
